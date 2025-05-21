9e92ca52bd271086a2d8eb57aea2f7fff6cc61c6
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-05-21 11:52:41 +0800

    Set up api gateway with custom domain and CDN

--- apps/infrastructure/comments.ts
+++ apps/infrastructure/comments.ts
@@ -6,6 +6,8 @@ const config = new pulumi.Config('comments')
 
 export class CommentsInfrastructure extends pulumi.ComponentResource {
     clusterEndpoint: pulumi.Output<string>
+    apiUrl: pulumi.Output<string>
+    cdnUrl: pulumi.Output<string>
 
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super('CommentsInfrastructure', name, {}, opts)
@@ -14,7 +16,10 @@ export class CommentsInfrastructure extends pulumi.ComponentResource {
             auroraDbName: config.require('db-name'),
             auroraDbUsername: config.require('db-username'),
             auroraDbPassword: config.requireSecret('db-password'),
+            domain: config.require('domain'),
+            subdomain: config.require('subdomain'),
         }
+        const domainName = pulumi.interpolate`${configObject.subdomain}.${configObject.domain}`
 
         // Create a new VPC
         const vpc = new aws.ec2.Vpc(`${name}-vpc`, {
@@ -77,6 +82,110 @@ export class CommentsInfrastructure extends pulumi.ComponentResource {
             },
         })
 
+        // Create the API Gateway
+        const api = new aws.apigatewayv2.Api(`${name}-apigateway`, {
+            protocolType: 'HTTP',
+        })
+
+        // Create default stage
+        new aws.apigatewayv2.Stage(`${name}-stage`, {
+            apiId: api.id,
+            name: '$default',
+            autoDeploy: true,
+        })
+
+        // Request existing certificate from ACM
+        // Cloudfront requires the certificate to be in the `us-east-1` region
+        const certificate = aws.acm.getCertificate(
+            {
+                domain: '*.codebykieran.com',
+                statuses: ['ISSUED'],
+                mostRecent: true,
+            },
+            {
+                provider: new aws.Provider(`${name}-cert-region-provider`, {
+                    region: 'us-east-1',
+                }),
+            },
+        )
+        // Get the ARN of the certificate
+        const certificateArn = certificate.then((cert) => cert.arn)
+
+        // Create a domain for the HTTP API
+        const commentsDomain = new aws.apigatewayv2.DomainName(
+            `${name}-custom-domain`,
+            {
+                domainName,
+                domainNameConfiguration: {
+                    certificateArn,
+                    endpointType: 'REGIONAL',
+                    securityPolicy: 'TLS_1_2',
+                },
+            },
+        )
+
+        // Set up CDN
+        const apiOriginName = `${name}-api-origin`
+        const cdn = new aws.cloudfront.Distribution(`${name}-distribution`, {
+            enabled: true,
+            aliases: [domainName],
+            priceClass: 'PriceClass_100',
+            restrictions: {
+                geoRestriction: {
+                    restrictionType: 'none',
+                },
+            },
+            viewerCertificate: {
+                acmCertificateArn: certificateArn,
+                sslSupportMethod: 'sni-only',
+                minimumProtocolVersion: 'TLSv1.2_2021',
+            },
+            origins: [
+                {
+                    domainName: commentsDomain.domainNameConfiguration.apply(
+                        (cfg) => cfg.targetDomainName,
+                    ),
+                    originId: apiOriginName,
+                    customOriginConfig: {
+                        originProtocolPolicy: 'https-only',
+                        httpPort: 80,
+                        httpsPort: 443,
+                        originSslProtocols: ['TLSv1.2'],
+                    },
+                },
+            ],
+            defaultCacheBehavior: {
+                allowedMethods: ['GET', 'HEAD'],
+                cachedMethods: ['GET', 'HEAD'],
+                targetOriginId: apiOriginName,
+                viewerProtocolPolicy: 'redirect-to-https',
+                forwardedValues: {
+                    queryString: false,
+                    cookies: { forward: 'none' },
+                    headers: [
+                        'Accept',
+                        'Accept-Encoding',
+                        'Access-Control-Allow-Origin',
+                        'Authorization',
+                        'Caller',
+                        'Content-Type',
+                        'Origin',
+                    ],
+                },
+                compress: true,
+            },
+        })
+
+        // Link the API Gateway to the custom domain
+        new aws.apigatewayv2.ApiMapping(`${name}-api-mapping`, {
+            apiId: api.id,
+            domainName: commentsDomain.domainName,
+            stage: '$default',
+            apiMappingKey: '',
+        })
+
         this.clusterEndpoint = cluster.endpoint
+        this.apiUrl = pulumi.interpolate`https://${domainName}/`
+        this.cdnUrl = cdn.domainName
     }
 }
--- apps/infrastructure/index.ts
+++ apps/infrastructure/index.ts
@@ -3,3 +3,5 @@ import { CommentsInfrastructure } from './comments'
 const comments = new CommentsInfrastructure('comments')
 
 export const clusterEndpoint = comments.clusterEndpoint
+export const apiUrl = comments.apiUrl
+export const cdnUrl = comments.cdnUrl
