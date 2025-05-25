479b952174f354b5a5dbdbcd3ea4a58f0c4e235e
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-05-25 10:22:48 +0800

    Fix certificate generation by using route 53

--- apps/infrastructure/index.ts
+++ apps/infrastructure/index.ts
@@ -5,3 +5,7 @@ const comments = new CommentsInfrastructure('comments')
 export const clusterEndpoint = comments.clusterEndpoint
 export const apiUrl = comments.apiUrl
 export const cdnUrl = comments.cdnUrl
+export const validationName = comments.validationName
+export const validationType = comments.validationType
+export const validationValue = comments.validationValue
+export const route53NameServers = comments.route53NameServers
--- apps/infrastructure/utils/types.ts
+++ apps/infrastructure/utils/types.ts
@@ -1,9 +1,9 @@
 import { Output } from '@pulumi/pulumi'
 
 export type ConfigType = {
-    auroraDbName: string
-    auroraDbUsername: string
-    auroraDbPassword: Output<string>
+    dbName: string
+    dbUsername: string
+    dbPassword: Output<string>
     domain: string
     subdomain: string
 }
