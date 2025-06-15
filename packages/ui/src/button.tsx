c5b7a1f22c225764bee2dec94940bb13bad53374
Jane Holborn <janeholborn@hotmail.com>
2025-06-15 09:54:25 +0800

    Created reusable button component.

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,28 +1,55 @@
 import './styles/button.css'
 
-type ButtonProps = {
-    backgroundColor: string
-    textColor: string
-    text: string
-    onClick: () => void
-}
+type ButtonProps = { onClick: () => void; type: ButtonType }
 
-export const Button = ({
-    backgroundColor,
-    textColor,
-    text,
-    onClick,
-}: ButtonProps) => {
+type ButtonType = 'approved' | 'rejected' | 'approve-neutral' | 'reject-neutral'
+
+export const Button = ({ onClick, type }: ButtonProps) => {
+    const { textColor, text, backgroundColor } = getButtonConfig(type)
     return (
         <button
-            onClick={onClick}
             style={{
                 borderColor: textColor,
                 background: backgroundColor,
                 color: textColor,
             }}
+            onClick={onClick}
         >
             {text}
         </button>
     )
+}
+
+type ButtonReturnType = {
+    text: string
+    backgroundColor: string
+    textColor: string
+}
+const getButtonConfig = (type: ButtonType): ButtonReturnType => {
+    switch (type) {
+        case 'approved':
+            return {
+                text: 'approved',
+                backgroundColor: '#D0F9D9',
+                textColor: '#034211',
+            }
+        case 'rejected':
+            return {
+                text: 'rejected',
+                backgroundColor: '#FFC3C7',
+                textColor: '#420000',
+            }
+        case 'approve-neutral':
+            return {
+                text: 'approve',
+                backgroundColor: 'none',
+                textColor: '#525252',
+            }
+        case 'reject-neutral':
+            return {
+                text: 'reject',
+                backgroundColor: 'none',
+                textColor: '#525252',
+            }
+    }
 }
