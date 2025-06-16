372dbcbc901d0bc68d2bbe1ad5d475129c03ff52
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-06-16 12:00:49 +0800

    Add additional icons

---
+++ packages/ui/src/icons/historyIcon.tsx
@@ -0,0 +1,19 @@
+export const HistoryIcon = () => {
+    return (
+        <svg
+            width="19"
+            height="18"
+            viewBox="0 0 19 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M1.95703 7.22222C1.95703 7.22222 3.64544 4.79397 5.0171 3.34511C6.38877 1.89624 8.28427 1 10.3781 1C14.5638 1 17.957 4.58172 17.957 9C17.957 13.4183 14.5638 17 10.3781 17C6.92285 17 4.00765 14.5594 3.09536 11.2222M1.95703 7.22222V1.88889M1.95703 7.22222H7.00966"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
