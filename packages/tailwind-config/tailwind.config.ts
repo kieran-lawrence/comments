95a7b681f7a787da5d55ca4498fbaa03324f88a7
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-06-20 16:26:41 +0800

    Fix tailwind config & add some more styles

---
+++ packages/tailwind-config/tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        '../ui/**/*.{js,ts,jsx,tsx}', // Adjust path as needed for your UI package
+        // Add other paths if needed
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
