405df6f19e5715a530d56ede44f633c9e26a295b
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-06-20 16:27:59 +0800

    Add some more required icons

---
+++ packages/ui/src/icons/checkIcon.tsx
@@ -0,0 +1,13 @@
+export const CheckIcon = () => {
+    return (
+        <svg viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M17 1L6 12L1 7"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
