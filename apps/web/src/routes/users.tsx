c29e01d47da04e4985d9bca2f473439c36206b17
Jane Holborn <janeholborn@hotmail.com>
2025-06-20 19:20:05 +0800

    Refactored users table to use tailwind and API data

--- apps/web/src/routes/users.tsx
+++ apps/web/src/routes/users.tsx
@@ -1,21 +1,35 @@
 import { PageLayout, UserTable } from '@repo/ui'
 import { createFileRoute } from '@tanstack/react-router'
+import { getUsers } from '../services/api'
+import { useQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/users')({
     component: UsersPage,
 })
 
 function UsersPage() {
+    const {
+        data: users,
+        isLoading,
+        isError,
+        // refetch,
+    } = useQuery({
+        queryKey: ['getUsersKey'],
+        queryFn: getUsers,
+    })
+
+    // TODO: Add loading and error states
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (isError || !users) {
+        return <div>Error loading users</div>
+    }
+
     return (
         <PageLayout
             sidebar={<>Sidebar</>}
-            mainContent={
-                <div style={{ width: '1100px', padding: '16px' }}>
-                    <UserTable type="active" />
-                    <UserTable type="suspended" />
-                    <UserTable type="banned" />
-                </div>
-            }
+            mainContent={<UserTable users={users} />}
         />
     )
 }
