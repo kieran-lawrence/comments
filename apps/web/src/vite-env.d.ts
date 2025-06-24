4a38f391e48b9d2f288995c887ad31c393132236
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-06-24 17:22:48 +0800

    Add type safety for env files

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string
+    readonly VITE_API_KEY: string
+    readonly VITE_AUTH0_CLIENT_ID: string
+    readonly VITE_AUTH0_DOMAIN: string
+    readonly VITE_SITE_NAME: string
+}
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
