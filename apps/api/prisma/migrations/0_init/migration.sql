fd6e2aa2ec0ea1a0ffea98e0a21287f45cae6565
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-06-24 08:51:41 +0800

    Delete site from schema & change userId to string to support auth0 id

--- apps/api/prisma/migrations/0_init/migration.sql
+++ apps/api/prisma/migrations/0_init/migration.sql
@@ -11,28 +11,18 @@ CREATE TYPE "CommentStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'FLAGGED
 CREATE TYPE "CommentStatusChangeBy" AS ENUM ('STAFF', 'COMMUNITY', 'SYSTEM');
 
 -- CreateTable
-CREATE TABLE "Site" (
+CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Site_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'USER',
-    "email" TEXT NOT NULL UNIQUE,
+    "email" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "siteId" TEXT NOT NULL,
     "active" BOOLEAN NOT NULL DEFAULT true,
     "deleted" BOOLEAN NOT NULL DEFAULT false,
     "suspended" BOOLEAN NOT NULL DEFAULT false,
     "suspendedAt" TIMESTAMP(3),
     "suspendedUntil" TIMESTAMP(3),
-    "suspendedById" INTEGER,
+    "suspendedById" TEXT,
     "suspendedReason" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -40,8 +30,8 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "UserIgnoredUsers" (
-    "userId" INTEGER NOT NULL,
-    "ignoredId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    "ignoredId" TEXT NOT NULL,
     "ignoredAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "UserIgnoredUsers_pkey" PRIMARY KEY ("userId","ignoredId")
@@ -55,8 +45,7 @@ CREATE TABLE "Article" (
     "articleTitle" TEXT NOT NULL,
     "status" "ArticleCommentingStatus" NOT NULL DEFAULT 'OPEN',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "authorId" INTEGER NOT NULL,
-    "siteId" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
 
     CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
 );
@@ -71,9 +60,9 @@ CREATE TABLE "Comment" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "articleId" INTEGER NOT NULL,
-    "authorId" INTEGER NOT NULL,
-    "reviewedById" INTEGER,
-    "flaggedById" INTEGER,
+    "authorId" TEXT NOT NULL,
+    "reviewedById" TEXT,
+    "flaggedById" TEXT,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
@@ -85,15 +74,15 @@ CREATE TABLE "CommentStatusChanges" (
     "oldStatus" "CommentStatus" NOT NULL,
     "newStatus" "CommentStatus" NOT NULL,
     "changedBy" "CommentStatusChangeBy" NOT NULL DEFAULT 'SYSTEM',
-    "changedById" INTEGER,
+    "changedById" TEXT,
     "changedReason" TEXT,
     "changedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "CommentStatusChanges_pkey" PRIMARY KEY ("id")
 );
 
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_siteId_fkey" FOREIGN KEY ("siteId") REFERENCES "Site"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_suspendedById_fkey" FOREIGN KEY ("suspendedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -106,9 +95,6 @@ ALTER TABLE "UserIgnoredUsers" ADD CONSTRAINT "UserIgnoredUsers_ignoredId_fkey" 
 
 -- AddForeignKey
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Article" ADD CONSTRAINT "Article_siteId_fkey" FOREIGN KEY ("siteId") REFERENCES "Site"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
