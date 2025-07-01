9183da72577b1ef2070685a0a7f081af8dc4584e
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-07-01 17:38:53 +0800

    Make filter component generic & add param to handle updating current selected item

--- packages/ui/src/filter.tsx
+++ packages/ui/src/filter.tsx
@@ -1,27 +1,33 @@
+import { FilterOption } from '@repo/shared-types'
 import './styles/filter.css'
+import { Dispatch, SetStateAction } from 'react'
 
-type FilterProps = {
+type FilterProps<T extends string> = {
     filterTitle: string
-    filterCount: Record<string, number | undefined>
-    activeItem: string
+    filterCount: Record<T, number | undefined>
+    activeItem: T
+    onClick: Dispatch<SetStateAction<T>>
 }
 
-export const Filter = ({
+export const Filter = <T extends FilterOption>({
     filterTitle,
     filterCount,
     activeItem,
-}: FilterProps) => {
+    onClick,
+}: FilterProps<T>) => {
     return (
         <div className="filterContainer">
             <h3 className="filterTitle">{filterTitle}</h3>
             <div className="filterOptions">
                 {Object.entries(filterCount).map(([filter, count]) => (
                     <button
-                        className={`filterButton ${activeItem === filter ? 'active' : ''}`}
+                        className={`cursor-pointer filterButton ${activeItem === filter ? 'active' : ''}`}
+                        onClick={() => onClick(filter as T)}
+                        key={filter}
                     >
-                        {count === undefined
-                            ? `${filter}`
-                            : `${filter} (${count})`}
+                        {count
+                            ? `${filter.toLowerCase()} (${count})`
+                            : `${filter.toLowerCase()}`}
                     </button>
                 ))}
             </div>
