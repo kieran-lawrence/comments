9c558311808b8b1418bffe98ce71ab433abd5109
Kieran Lawrence <149361698+kieran-lawrence@users.noreply.github.com>
2025-07-03 08:04:54 +0800

    Feature: Add Filtering, Searching and Sorting (#18)
    * Update status changes API response to include article and author
    
    * Remove site references from type definitions
    
    * Add search logic to search component & move styles to tailwind
    
    * Make filter component generic & add param to handle updating current selected item
    
    * Move filter to tailwind too
    
    * Pass userId to commentCard, and fix link styling
    
    * Add extra types for filtering options
    
    * Add filters/sorts to all pages

--- apps/web/src/routes/_auth.users.tsx
+++ apps/web/src/routes/_auth.users.tsx
@@ -1,7 +1,26 @@
-import { ErrorComponent, LoadingOverlay, PageLayout, UserTable } from '@repo/ui'
+import {
+    ErrorComponent,
+    Filter,
+    LoadingOverlay,
+    PageLayout,
+    Search,
+    UserTable,
+} from '@repo/ui'
 import { createFileRoute } from '@tanstack/react-router'
 import { getUsers } from '../services/api'
 import { useQuery } from '@tanstack/react-query'
+import {
+    SortOptions,
+    RoleFilterOptions,
+    AccountStatusOptions,
+} from '@repo/shared-types'
+import { useMemo, useState } from 'react'
+import {
+    filterUsersByRole,
+    filterUsersByStatus,
+    searchUsers,
+    sortUsers,
+} from '../utils/helpers'
 
 export const Route = createFileRoute('/_auth/users')({
     component: UsersPage,
@@ -22,11 +41,30 @@ function UsersPage() {
         queryKey: ['getUsersKey'],
         queryFn: getUsers,
     })
+    const [roleFilter, setRoleFilter] = useState<RoleFilterOptions>('ALL')
+    const [statusFilter, setStatusFilter] =
+        useState<AccountStatusOptions>('Active')
+    const [sort, setSort] = useState<SortOptions>('Newest First')
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchFilteredUsers = useMemo(() => {
+        if (!users) return null
+        const filteredByAccountStatus = filterUsersByStatus(
+            sortUsers(users, sort),
+            statusFilter,
+        )
+        const filteredByRole = filterUsersByRole(
+            filteredByAccountStatus,
+            roleFilter,
+        )
+        if (!searchTerm) return filteredByRole
+        return searchUsers(filteredByRole, searchTerm)
+    }, [users, searchTerm, statusFilter, sort, roleFilter])
 
     if (isLoading) {
         return <LoadingOverlay />
     }
-    if (isError || !users) {
+    if (isError || !searchFilteredUsers) {
         throw new Error(
             `Encountered an issue while fetching user data.\n Please try again later or contact us if the problem persists.`,
             {
@@ -37,8 +75,46 @@ function UsersPage() {
 
     return (
         <PageLayout
-            sidebar={<>Sidebar</>}
-            mainContent={<UserTable users={users} />}
+            sidebar={
+                <div className="flex flex-col items-left gap-6">
+                    <Search
+                        placeholder="Search by user name or email"
+                        onSearch={setSearchTerm}
+                    />
+                    <Filter<RoleFilterOptions>
+                        filterTitle="Role"
+                        filterCount={{
+                            ALL: undefined,
+                            USER: undefined,
+                            MODERATOR: undefined,
+                            ADMIN: undefined,
+                        }}
+                        activeItem={roleFilter}
+                        onClick={setRoleFilter}
+                    />
+                    <Filter<AccountStatusOptions>
+                        filterTitle="Account Status"
+                        filterCount={{
+                            Active: undefined,
+                            Suspended: undefined,
+                            Banned: undefined,
+                            Deleted: undefined,
+                        }}
+                        activeItem={statusFilter}
+                        onClick={setStatusFilter}
+                    />
+                    <Filter<SortOptions>
+                        filterTitle="Sort"
+                        filterCount={{
+                            'Newest First': undefined,
+                            'Oldest First': undefined,
+                        }}
+                        activeItem={sort}
+                        onClick={setSort}
+                    />
+                </div>
+            }
+            mainContent={<UserTable users={searchFilteredUsers} />}
         />
     )
 }
--- packages/ui/src/search.tsx
+++ packages/ui/src/search.tsx
@@ -1,17 +1,29 @@
-import './styles/search.css'
 import { SearchIcon } from './icons/searchIcon'
 
 type SearchProps = {
     placeholder: string
+    onSearch: (searchTerm: string) => void
 }
 
-export const Search = ({ placeholder }: SearchProps) => {
+export const Search = ({ placeholder, onSearch }: SearchProps) => {
+    const handleChange = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const form = event.currentTarget
+        const searchField = form.elements.namedItem(
+            'searchField',
+        ) as HTMLInputElement
+        onSearch(searchField.value.trim())
+    }
     return (
-        <form className="searchContainer">
-            <div className="searchContent">
+        <form
+            onChange={handleChange}
+            onSubmit={(e) => e.preventDefault()}
+            className="textTitleItemSm flex flex-col px-4 w-full justify-center bg-bg-card border border-bg-card-alt rounded-md"
+        >
+            <div className="flex items-center gap-2">
                 <SearchIcon />
                 <input
-                    className="input"
+                    className="flex py-3 grow items-center placeholder:color-border-primary bg-bg-card outline-0"
                     type="text"
                     placeholder={placeholder}
                     name="searchField"
